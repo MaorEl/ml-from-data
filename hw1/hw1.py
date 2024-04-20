62fd43dd1ec0c58b80fe040b97825ee5a2d69174
MaorE <maorelfassy@gmail.com>
2024-04-20 19:13:50 +0300

    maor

--- hw1/hw1.py
+++ hw1/hw1.py
@@ -1,6 +1,6 @@
 ###### Your ID ######
-# ID1: 123456789
-# ID2: 987654321
+# ID1: 204226815
+# ID2: 987654321 Todo Tzachi - change to your ID
 #####################
 
 # imports 
@@ -74,33 +74,52 @@ def apply_bias_trick(X):
 
     return X_bias
 
+
 # n - number of  instances(row - 0)
 # m - number of features(col -1)
 def compute_cost(X, y, theta):
     number_of_instances = X.shape[0]
     number_of_features = X.shape[1]
 
-    if(len(theta) != number_of_features):
+    if len(theta) != number_of_features:
         raise Exception("Theta must be a vector with a size equal to the number of features.")
-    if(len(y) != number_of_instances):
+    if len(y) != number_of_instances:
         raise Exception("True labels must be a vector with a size equal to the number of instances.")
 
-    h_teta = X @ theta
-    sqrErrors = np.square(h_teta - y )
+    h_theta = X @ theta
+    squared_errors = np.square(h_theta - y)
     
-    return  1 / (2 * number_of_instances) * np.sum(sqrErrors)
-
+    return 1 / (2 * number_of_instances) * np.sum(squared_errors)
 
 
 def gradient_descent(X, y, theta, alpha, num_iters):
+    """
+    Learn the parameters of the model using gradient descent using
+    the training set. Gradient descent is an optimization algorithm
+    used to minimize some (loss) function by iteratively moving in
+    the direction of steepest descent as defined by the negative of
+    the gradient. We use gradient descent to update the parameters
+    (weights) of our model.
+
+    Input:
+    - X: Input data (m instances over n features).
+    - y: True labels (m instances).
+    - theta: The parameters (weights) of the model being learned.
+    - alpha: The learning rate of your model.
+    - num_iters: The number of updates performed.
+
+    Returns:
+    - theta: The learned parameters of your model.
+    - J_history: the loss value for every iteration.
+    """
     number_of_instances = X.shape[0]
-    J_array = np.empty(num_iters)
+    J_history = np.empty(num_iters)
     theta_copy = np.copy(theta)
     for iteration in range(num_iters):
-        theta_copy =generate_new_teta(X, y, alpha, number_of_instances, theta_copy)
-        J_array[iteration] = compute_cost(X, y, theta_copy)
+        theta_copy = generate_new_theta(X, y, alpha, number_of_instances, theta_copy)
+        J_history[iteration] = compute_cost(X, y, theta_copy)
 
-    return theta_copy, J_array
+    return theta_copy, J_history
 
 
 
@@ -151,21 +170,22 @@ def efficient_gradient_descent(X, y, theta, alpha, num_iters):
     theta_copy = np.copy(theta)
     prev_j = compute_cost(X, y, theta_copy)
     for iteration in range(num_iters):
-        theta_copy =generate_new_teta(X, y, alpha, number_of_instances, theta_copy)
+        theta_copy = generate_new_theta(X, y, alpha, number_of_instances, theta_copy)
         J_history[iteration] = compute_cost(X, y, theta_copy)
-        if(prev_j - J_history[iteration] < 1e-8):
+        if prev_j - J_history[iteration] < 1e-8:
             print(f"for alpha: {alpha} breaking in iteration: {iteration}. when theta: {theta_copy}. origin theta:{theta} when cost change: {prev_j - J_history[iteration]}")
             break
         prev_j = J_history[iteration]
 
-
     return theta_copy, J_history
 
-def generate_new_teta(X, y, alpha, number_of_instances, theta_copy):
-    h_teta = X @ theta_copy
-    errors = h_teta - y
-    theta_copy -= (alpha/number_of_instances) * (X.T @ errors)
-    return theta_copy
+
+def generate_new_theta(X, y, alpha, number_of_instances, theta):
+    h_theta = X @ theta
+    errors = h_theta - y
+    theta -= (alpha / number_of_instances) * (X.T @ errors) # todo maor - why X.T?
+
+    return theta
 
 def find_best_alpha(X_train, y_train, X_val, y_val, iterations):
     """
@@ -209,13 +229,48 @@ def forward_feature_selection(X_train, y_train, X_val, y_val, best_alpha, iterat
     - selected_features: A list of selected top 5 feature indices
     """
     selected_features = []
-    #####c######################################################################
     # TODO: Implement the function and find the best alpha value.             #
-    ###########################################################################
-    pass
-    ###########################################################################
-    #                             END OF YOUR CODE                            #
-    ###########################################################################
+    '''
+    Adding additional features to our regression model makes it more complicated but does not necessarily improves performance. 
+    
+    Forward feature selection is a greedy, iterative algorithm used to select the most relevant features for a predictive model. The objective of this algorithm is to improve the model's performance by identifying and using only the most relevant features, potentially reducing overfitting, improving accuracy, and reducing computational cost. 
+    
+    Implement forward feature selection using the following guidelines: 
+    1. Start with an empty set of selected features.
+    1. For each feature not yet in the selected set, do the following:
+        1. Add the feature to the selected set temporarily.
+        1. Train a model using the current set of selected features and evaluate its performance by calculating the cost or error on a validation set.
+        1. Remove the temporarily added feature from the selected set.
+    1. Choose the feature that resulted in the best model performance and permanently add it to the selected set.
+    1. Repeat steps 2-3 until you have 5 features (not including the bias).
+    '''
+    number_of_features = X_train.shape[1]
+    X_train = apply_bias_trick(X_train)
+    X_val = apply_bias_trick(X_val)
+    best_cost = np.inf
+    best_feature = None
+
+    for i in range(5):
+        for feature in range(number_of_features):
+
+            if feature in selected_features:
+                continue
+
+            selected_features.append(feature)
+            theta = np.random.random(size=len(selected_features) + 1)  # adding 1 for bias
+            x_train_to_use = X_train[:, [0] + selected_features]
+            x_val_to_use = X_val[:, [0] + selected_features]
+            gradient_descent_result = efficient_gradient_descent(x_train_to_use, y_train, theta, best_alpha, iterations)
+            cost = compute_cost(x_val_to_use, y_val, gradient_descent_result[0])
+
+            if cost < best_cost:
+                best_cost = cost
+                best_feature = feature
+
+            selected_features.remove(feature)
+
+        selected_features.append(best_feature)
+
     return selected_features
 
 def create_square_features(df):
